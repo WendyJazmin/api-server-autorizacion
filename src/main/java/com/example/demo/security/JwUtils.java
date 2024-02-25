46c6d49dab49397b08c39f92a58e5c95ff08404b
wendyjazmin <wendyjazmin1994@hotmail.com>
2024-02-25 12:48:05 -0500

    taller41: actualizando la semilla

--- src/main/java/com/example/demo/security/JwUtils.java
+++ src/main/java/com/example/demo/security/JwUtils.java
@@ -20,9 +20,9 @@ public class JwUtils {
 	private int jwtExpirationMs;
 	
 	public String buildTokenJwt(String nombre) {
-		return Jwts.builder().setSubject(nombre).setSubject("Hola mundo").setIssuedAt(new Date())
-				.setExpiration(new Date(System.currentTimeMillis() + this.jwtExpirationMs))
-				.signWith(SignatureAlgorithm.HS512, "semillawdefrfgdtvgbomgobmdrb203290324825835824202405385358035edscfsdfsdfds3053035")
+		return Jwts.builder().setSubject(nombre).setIssuedAt(new Date())
+				.setExpiration(new Date(System.currentTimeMillis() +this.jwtExpirationMs/*+ 100000000*/))//this.jwtExpirationMs
+				.signWith(SignatureAlgorithm.HS512, "fckmsdmfmisofsfosofifisdefikiregi9483r84ruff8ufru87rg8ww322rrrrfsemillawdefrfgdtvgbomgobmdrb203290324825835824202405385358035edscfsdfsdfds3053035")
 				.compact();//setSubject se pude oner varias veces y enviar lo que queramos
 		
 	}
